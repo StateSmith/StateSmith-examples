321cd459570e5c5db37990a1bc9c3d17fd68a57f
Mike Burton <mb-github@niskala.org>
2024-05-11 07:29:37 -0700

    minor

--- test-gen-0/src/LightSm.jest.test.js
+++ test-gen-0/src/LightSm.jest.test.js
@@ -10,18 +10,17 @@ import {jest} from '@jest/globals'; // We'll use jest in this example. see packa
 globalThis.println = jest.fn();
 globalThis.light_blue = jest.fn();
 
-test('println to be called once on startup', () => {
-    const sm = new LightSm();
-    sm.start();
-    expect(globalThis.println.mock.calls).toHaveLength(1);
-});
-
 test('starts in the off state', () => {
     const sm = new LightSm();
     sm.start();
     expect(sm.stateId).toBe(LightSm.StateId.OFF);
 });
 
+test('println to be called once on startup', () => {
+    const sm = new LightSm();
+    sm.start();
+    expect(globalThis.println.mock.calls).toHaveLength(1);
+});
 
 test('light is blue when turned on', () => {
     const sm = new LightSm();

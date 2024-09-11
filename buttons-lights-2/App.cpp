323415034938805d22a1dad98bb65e25b7b148af
Adam Fraser-Kruck <adamfk@gmail.com>
2024-09-10 23:34:52 -0500

    button-lights-2 platformio

---
+++ buttons-lights-2/App.cpp
@@ -0,0 +1,30 @@
+#include "App.hpp"
+
+#include <Arduino.h>
+#include <stdint.h>
+#include "LightController.h"
+
+// Millisecond time when loop() was ran last.
+// This is used to calculate the elapsed time between loop iterations.
+// Tracking this value here instead of inside of each button state machine
+// allows us to save some RAM. Important if you have lots of buttons.
+// You could easily add this to the button state machine if you prefer.
+static uint32_t last_loop_ms = 0;
+
+void App_setup()
+{
+    Serial.begin(115200);
+    Serial.println(F("Starting!"));
+    LightController_setup();
+}
+
+void App_loop()
+{
+    // calculate milliseconds since the last time our loop ran.
+    const uint32_t now_ms = millis();
+    const uint32_t elapsed_time_ms = now_ms - last_loop_ms;
+    last_loop_ms = now_ms;
+  
+    LightController_update(elapsed_time_ms);
+    delay(5);   // delay decreases wokwi simulation CPU usage
+}
---
+++ buttons-lights-2/App.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+void App_setup();
+void App_loop();
